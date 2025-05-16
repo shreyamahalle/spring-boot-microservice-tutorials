5a55da54722cad556528581e954cd121dca2d52f
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-16 19:56:12 +0530

    12 factor app example

---
+++ twelve-factor-spring-boot-app/src/main/java/com/shreya/twelvefactorapp/TwelveFactorSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.twelvefactorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveFactorSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwelveFactorSpringBootAppApplication.class, args);
+	}
+
+}
